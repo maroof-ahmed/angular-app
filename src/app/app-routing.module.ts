9ba0bf3f8edafee474fea75a98eb862d20744a69
maroof-ahmed <mav_rock@hotmail.com>
2021-08-09 21:44:40 +0500

    develop: Added multiple components and routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Routes
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductComponent } from './product/product.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/category/beverages', pathMatch: 'full' },
+  {
+    path: 'category',
+    component: ProductListingComponent,
+    children: [
+      {
+        path: '**', // child route path
+        component: ProductComponent, // child route component that the router renders
+      },
+    ],
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductComponent } from './product/product.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    ProductListingComponent,
+    ProductComponent,
+    BreadcrumbComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
